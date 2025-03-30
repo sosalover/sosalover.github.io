d60d32450aed8b49212d0eddf7fb3af31da776f7
tmoh-figs <150082683+tmoh-figs@users.noreply.github.com>
2025-03-30 15:24:54 -0400

    fix: facebook link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
     </p>
     <div class="mt-8">
       <a
-        href="facebook.com"
+        href="www.facebook.com"
         download="ThomasMoh_Resume"
         class="bg-blue-500 text-white py-2 px-6 rounded hover:bg-blue-600"
         >Download Resume</a

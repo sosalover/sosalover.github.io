fba53dee3371b6d307091b1281facc27cb5698d4
tmoh-figs <150082683+tmoh-figs@users.noreply.github.com>
2025-03-30 15:04:36 -0400

    fix: use app dir

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import {
-    vitePreprocess
-} from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
     kit: {
-        adapter: adapter(),
-        
-    }
+        appDir: 'app', // Required as the default is _app
+        adapter: adapter()
+    },
+    preprocess: vitePreprocess()
 };
-
 export default config;
